ab44d731e68fa47edb4e46e1d091d8918d693e12
vyaspnk <vmalla@Vijethas-MacBook-Air.local>
2017-02-26 19:56:58 -0800

    Trees absolute difference

---
+++ Trees/Trees/Minimum_Absolute_difference.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+private:
+    void buildInOrder(TreeNode *root, vector<int> &order) {
+        if(!root)
+            return;
+        if(root->left)
+            buildInOrder(root->left,order);
+        order.push_back(root->val);
+        if(root->right)
+            buildInOrder(root->right,order);
+        return;
+    }
+public:
+    int getMinimumDifference(TreeNode* root) {
+        vector<int> order;
+        buildInOrder(root, order);
+        int adiff = INT_MAX;
+        for(auto i = 0;i < order.size()-1; i++) {
+            cout << order[i] << " ";
+            auto delta = abs(order[i]-order[i+1]);
+            if(delta < adiff)
+                adiff = delta;
+        }
+        return adiff;
+    }
+};
